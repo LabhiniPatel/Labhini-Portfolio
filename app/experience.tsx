a1a349865f0cd179511d959d1fd70e11996c239e
Labhini Patel <patel.la@northeastern.edu>
2026-01-11 23:56:51 -0500

    Update experience.tsx

--- app/experience.tsx
+++ app/experience.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import { Brain, BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 export default function Experience() {
   const experiences = [
